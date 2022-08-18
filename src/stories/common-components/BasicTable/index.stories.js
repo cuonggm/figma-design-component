faf2addf52bca9720f20c957265a1ab3111f6723
CuongGM <cuonggm48@gmail.com>
2022-08-18 23:59:23 +0700

    Add BasicTable

---
+++ src/stories/common-components/BasicTable/index.stories.js
@@ -0,0 +1,14 @@
+import BasicTable from "../../../common-components/BasicTable";
+
+export default {
+    title: "BasicTable",
+    component: BasicTable,
+}
+
+const Template = (args) => <BasicTable {...args}/>
+
+export const DefaultTable = {
+    args: {
+
+    }
+}
